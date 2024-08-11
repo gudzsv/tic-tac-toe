d15e5595f67a356b2a4898ade265a02eb3625649
gudzsv <gudzsvmail@gmail.com>
2024-08-11 12:43:57 +0300

    added basic players area

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<li>
+						<span className='player-name'>Player 1</span>
+						<span className='player-symbol'>X</span>
+					</li>
+					<li>
+						<span className='player-name'>Player 2</span>
+						<span className='player-symbol'>0</span>
+					</li>
+				</ol>
+			</div>
+		</main>
+	);
+}
+
+export default App;
