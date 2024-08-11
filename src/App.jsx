3202b1dcd244941c0cbb1b1a15dd397e1b3e8d59
gudzsv <gudzsvmail@gmail.com>
2024-08-11 14:39:06 +0300

    added Played component with two way binding player name and buttons to edit and save value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Player from './components/Player.jsx';
+
 function App() {
 	return (
 		<main>
 			<div id='game-container'>
 				<ol id='players'>
-					<li>
-						<span className='player-name'>Player 1</span>
-						<span className='player-symbol'>X</span>
-					</li>
-					<li>
-						<span className='player-name'>Player 2</span>
-						<span className='player-symbol'>0</span>
-					</li>
+					<Player initialName='Player 1' symbol='X' />
+					<Player initialName='Player 2' symbol='0' />
 				</ol>
 			</div>
 		</main>
